8e37172d1c0bb0175cbfe474a70ea2ac00227139
TheRiser2001 <michi.ilic@hotmail.com>
2025-04-14 11:59:52 +0200

    Initial Commit

---
+++ FridgeAI/FridgeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeAIApp.swift
+//  FridgeAI
+//
+//  Created by Michael Ilic on 14.04.25.
+//
+
+import SwiftUI
+
+@main
+struct FridgeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
